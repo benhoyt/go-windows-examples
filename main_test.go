7635477588ef97c18cb05c70c8c8fa1cd52e60fc
Ben Hoyt <benhoyt@gmail.com>
2022-02-19 16:24:21 +1300

    add other examples

--- main_test.go
+++ main_test.go
@@ -1,16 +1,31 @@
 package main_test
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/benhoyt/go-windows-examples/lib"
 )
 
-func Example() {
+func Example1() {
 	err := lib.WriteOutput(os.Stdout, "foo\nbar\n")
 	if err != nil {
 		panic(err)
 	}
+	// Output:
+	// foo
+	// bar
+}
+
+func Example2() {
+	fmt.Print("foo\nbar\n")
+	// Output:
+	// foo
+	// bar
+}
+
+func Example3() {
+	fmt.Print("foo\r\nbar\r\n")
 	// Output:
 	// foo
 	// bar
